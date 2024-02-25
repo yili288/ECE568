08088b9f22749466acfd4c3d34a8233aed06e97a
Yi Li  Ng <ngyi4@remote.ecf.utoronto.ca>
2024-02-25 18:49:01 -0500

    working validate dr code

--- lab2/part1/validateQRcode.c
+++ lab2/part1/validateQRcode.c
@@ -11,23 +11,25 @@
 #define DIGITS_POWER 1000000
 
 // Function to convert a HEX string to binary value in unsigened char variable
+// reference: https://stackoverflow.com/questions/3408706/hexadecimal-string-to-byte-array-in-c
 uint8_t* hexStr2Bytes(const char *hex_str){
 	uint8_t bytearray_size = strlen(hex_str) / 2;
 	uint8_t *bytearray = malloc(bytearray_size * sizeof(unsigned char));
-
+	
 	for (int i = 0; i < bytearray_size; i++)
 	{
 		sscanf(hex_str + 2 * i, "%02x", &bytearray[i]);
 	}
+
 	return bytearray;
 }
 
 // Function to truncate a hash value to 6 characters
 // base on RFC docs page 12,13
-int truncateHash(const uint8_t *hash, int hashLength)
+int truncateHash(uint8_t *hash, int hashLength)
 {
 	// Calculate the offset from the last byte of the hash
-	int offset = hash[hashLength - 1] & 0x0F;
+	int offset = hash[hashLength - 1] & 0xF;
 
 	// Calculate the binary value using the selected bytes
 	int binary =
@@ -43,24 +45,22 @@ int truncateHash(const uint8_t *hash, int hashLength)
 }
 
 // Function to perform XOR operation between two binary values
-void xorBinary(uint8_t *binary1, uint8_t *repeat_binary, uint8_t *result, size_t length)
+void xorBinary(const uint8_t *binary1, uint8_t *repeat_binary, uint8_t *result, size_t length)
 {
-	for (size_t i = 0; i < length; ++i)
+	for (int i = 0; i < length; i++)
 	{
 		result[i] = binary1[i] ^ *repeat_binary; // Perform XOR with repeating value
 	}
 }
 
 // calls the sha1 functions to hash the inner or outer key with a message
-uint8_t *hash(uint8_t *key, uint8_t *message, uint8_t *sha)
+uint8_t *hash(uint8_t *key, int key_size, uint8_t *message, int m_size, uint8_t *sha)
 {
 	SHA1_INFO ctx;
 	sha1_init(&ctx);
 
-	sha1_update(&ctx, key, strlen(key));
-	sha1_update(&ctx, message, strlen(message));
-	printf("size of key: %d\n", strlen(key));
-	printf("size of message: %d\n", strlen(message));
+	sha1_update(&ctx, key, key_size);
+	sha1_update(&ctx, message, m_size);
 
 	sha1_final(&ctx, sha);
 
@@ -73,22 +73,25 @@ validateTOTP(char * secret_hex, char * TOTP_string)
 	// key to binary
 	uint8_t *secret_binary = hexStr2Bytes(secret_hex);
 	uint8_t secret_binary_padded[SHA1_BLOCKSIZE];
-	// pad 0s to the right of secret binary
-	for (int i = 0; i < SHA1_BLOCKSIZE; i++)
-	{
-		if (i < 10)
-			secret_binary_padded[i] = secret_binary[i];
-		else
-			secret_binary_padded[i] = 0;
-	}
-	printf("Secret (Binary): %u, size: %d\n", secret_binary_padded, strlen(secret_binary_padded));
+	// pad 0s to the right of secret binary until it reaches 64 bytes
+	memcpy(secret_binary_padded, secret_binary, strlen(secret_binary));
+	memset(secret_binary_padded + strlen(secret_binary), 0, SHA1_BLOCKSIZE - strlen(secret_binary));
+
+	// uint8_t *repeating_value1 = hexStr2Bytes("5c5c5c5c5c5c5c5c5c5c"); // inner pad
+	// uint8_t *repeating_value2 = hexStr2Bytes("36363636363636363636"); // outer pad
+	// printf("Repeating Value 1: %u\n", repeating_value1);
+	// printf("Repeating Value 2: %u\n", repeating_value2);
+
+	uint8_t outer_key[SHA1_BLOCKSIZE];
+	xorBinary(secret_binary_padded, (uint8_t *) "\x5c", outer_key, SHA1_BLOCKSIZE);
+
+	uint8_t inner_key[SHA1_BLOCKSIZE];
+	xorBinary(secret_binary_padded, (uint8_t *) "\x36", inner_key, SHA1_BLOCKSIZE);
 
 	// 30s timestamp message
-	struct tm *ptr;
 	time_t current_time;
 	current_time = time(NULL);
-	long T = current_time / 30;
-
+	time_t T = current_time / 30;
 	uint8_t time_binary[8];
 	for (int i = 7; i >= 0; i--)
 	{
@@ -96,26 +99,14 @@ validateTOTP(char * secret_hex, char * TOTP_string)
 		T >>= 8;
 	}
 
-	// uint8_t *repeating_value1 = hexStr2Bytes("5c5c5c5c5c5c5c5c5c5c"); // inner pad
-	// uint8_t *repeating_value2 = hexStr2Bytes("36363636363636363636"); // outer pad
-	// printf("Repeating Value 1: %u\n", repeating_value1);
-	// printf("Repeating Value 2: %u\n", repeating_value2);
-
-	uint8_t inner_key[SHA1_BLOCKSIZE];
-	xorBinary(secret_binary_padded, "\x5c", inner_key, 80);
-
 	uint8_t inner_hash[SHA1_DIGEST_LENGTH];
-	hash(inner_key, time_binary, inner_hash);
-	printf("Inner Hash: %u\n", inner_hash);
+	hash(inner_key, SHA1_BLOCKSIZE, time_binary, 8, inner_hash);
 
-	uint8_t outer_key[SHA1_BLOCKSIZE];
-	xorBinary(secret_binary_padded, "\x36", outer_key, 80);
+	SHA1_INFO ctx2;
 	uint8_t outer_hash[SHA1_DIGEST_LENGTH];
-	hash(outer_key, inner_hash, outer_hash);
-	printf("Outer Hash: %u\n", outer_hash);
+	hash(outer_key, SHA1_BLOCKSIZE, inner_hash, SHA1_DIGEST_LENGTH, outer_hash);
 
 	int output = truncateHash(outer_hash, SHA1_DIGEST_LENGTH);
-	printf("Current TOTP: %d\n", output);
 
 	if (output == atoi(TOTP_string))
 		return 1;
