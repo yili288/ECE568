a1072d6900be4cfedeb4fa42bca041246642cec1
Chengjie  Li <jefflicj@gmail.com>
2024-02-25 01:29:48 -0500

    Make use of encoding.c functions

--- lab2/part1/generateQRcode.c
+++ lab2/part1/generateQRcode.c
@@ -9,64 +9,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Function to convert a single hexadecimal digit to its binary equivalent
-char* hex_digit_to_bin(char hex_digit) {
-    switch(hex_digit) {
-        case '0': return "0000";
-        case '1': return "0001";
-        case '2': return "0010";
-        case '3': return "0011";
-        case '4': return "0100";
-        case '5': return "0101";
-        case '6': return "0110";
-        case '7': return "0111";
-        case '8': return "1000";
-        case '9': return "1001";
-        case 'a': case 'A': return "1010";
-        case 'b': case 'B': return "1011";
-        case 'c': case 'C': return "1100";
-        case 'd': case 'D': return "1101";
-        case 'e': case 'E': return "1110";
-        case 'f': case 'F': return "1111";
-        default: return NULL; // Invalid hexadecimal digit
-    }
-}
-
-// Function to convert a hexadecimal string to binary
-char* hex_to_bin(const char* hex_string) {
-    size_t hex_len = strlen(hex_string);
-    size_t bin_len = hex_len * 4; // 4 bits for each hexadecimal digit
-
-    // Allocate memory for binary string
-    char* binary_string = (char*)malloc(bin_len + 1); // +1 for null terminator
-    // if (binary_string == NULL) {
-    //     perror("Memory allocation failed");
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // Convert each hexadecimal digit to binary and concatenate
-    for (size_t i = 0; i < hex_len; i++) {
-        char* bin = hex_digit_to_bin(hex_string[i]);
-        // if (bin == NULL) {
-        //     fprintf(stderr, "Invalid hexadecimal digit: %c\n", hex_string[i]);
-        //     free(binary_string);
-        //     exit(EXIT_FAILURE);
-        // }
-        strcat(binary_string, bin);
-    }
-
-    return binary_string;
-}
-
-// int main() {
-//     const char* hex_string = "1A3F"; // Example hexadecimal string
-//     char* binary_string = hex_to_bin(hex_string);
-//     printf("Hexadecimal: %s\n", hex_string);
-//     printf("Binary: %s\n", binary_string);
-//     free(binary_string); // Free allocated memory
-//     return 0;
-// }
-
 int
 main(int argc, char * argv[])
 {
@@ -87,35 +29,40 @@ main(int argc, char * argv[])
 	// Create an otpauth:// URI and display a QR code that's compatible
 	// with Google Authenticator
 
-    int secret_len = strlen(secret_hex);
+    int secret_len = strlen(secret_hex); // always 20
     printf("secret_hex len: %d\n", secret_len);
-	char* binary_secret = hex_to_bin(secret_hex); // convert from hex to binary first
 
-    char encoded_binary[100] = {'\0'};
-    base32_encode(binary_secret, secret_len, encoded_binary, 100);
+    // char decoded_base32[20] = {'\0'}; // right way to initialize result array?
+    uint8_t decoded_base32[20];
+    base32_decode(secret_hex, decoded_base32, 20); // convert from hex to binary first
 
-    printf("encoded_binary: %s\n", encoded_binary);
+    printf("decoded_base32: %s\n", decoded_base32);
+
+    // char encoded_base32[80] = {'\0'}; // right way to initialize result array?
+    uint8_t encoded_base32[80];
+    base32_encode(decoded_base32, secret_len, encoded_base32, 80);
+
+    printf("encoded_base32: %s\n", encoded_base32);
+    
 	//  otpauth://totp/ACCOUNTNAME?issuer=ISSUER&secret=SECRET&period=30
 	char uri[] = "otpauth://totp/";
 	printf("%s\n", uri);
-	strcat(uri, accountName);
+	strcat(uri, urlEncode(accountName));
 	printf("%s\n", uri);
 	strcat(uri, "?issuer=");
 	printf("%s\n", uri);
-	strcat(uri, issuer);
+	strcat(uri, urlEncode(issuer));
 	printf("%s\n", uri);
 	strcat(uri, "&secret=");
 	printf("%s\n", uri);
-	strcat(uri, encoded_binary);
+	strcat(uri, encoded_base32);
 	printf("%s\n", uri);
 	strcat(uri, "&period=30");
-
-	free(binary_secret);
 
 	printf("%s\n", uri);
 
 	displayQRcode(uri);
-    
+
     // char correct_uri[] = "otpauth://totp/gibson?issuer=ECE568&secret=CI2FM6EQCI2FM6EQ&period=30";
     // displayQRcode(correct_uri);
 	// displayQRcode("otpauth://testing");
